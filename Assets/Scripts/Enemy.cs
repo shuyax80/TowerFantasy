fb23a46e367a40be667703a23ae01ecb24933fba
shuyax80 <66852198+shuyax80@users.noreply.github.com>
2026-05-10 13:05:00 +0200

    202605101304

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,10 +8,9 @@ public class Enemy : MonoBehaviour
     [SerializeField] private long scoreValue; 
     [SerializeField] private long damage;
     [SerializeField] private GameObject explosion;
+    [SerializeField] private Vector2 speedRange = new Vector2(0.3f, 1f);
     private float _speed;
-    private float _stopDistance;
     private float _rotationSpeed;
-    private Transform _playerTransform;
 
     private void Awake()
     {
@@ -35,30 +34,24 @@ public class Enemy : MonoBehaviour
     {
         var randomDirection = Random.Range(0, 100);
        
-        _speed = Random.Range(0.3f, 1.0f);
-        _stopDistance = 0.5f;
+        _speed = Random.Range(speedRange.x, speedRange.y);
         _rotationSpeed = randomDirection < 50 ? Random.Range(70f, 200f) : Random.Range(-200f, -70f);
-        _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     private void Update()
     {
-        var distance = Vector2.Distance(transform.position, _playerTransform.position);
-
-        if (distance > _stopDistance)
-        {
-            transform.position = Vector2.MoveTowards(
-                transform.position, 
-                _playerTransform.position, 
-                _speed * Time.deltaTime
-            );
-        }
-        else
-        {
-            Player.Instance.TakeDamage(damage);
-            DestroyEnemy();
-        }
+        transform.position += Vector3.down * (_speed * Time.deltaTime);
         transform.Rotate(0, 0, _rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent<Player>(out var player))
+        {
+            player.TakeDamage(damage);
+        }
+        
+        DestroyEnemy();
     }
 
     public void GetDamage(long amount)
